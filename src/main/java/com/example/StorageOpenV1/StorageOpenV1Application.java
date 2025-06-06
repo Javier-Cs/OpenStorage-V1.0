b6d4408061aa9b809ad6f3c777839dbfb8084a07
Javier-Cs <krchipullajavier@gmail.com>
2025-06-06 16:09:58 -0500

    carga de proyecto vacio

---
+++ src/main/java/com/example/StorageOpenV1/StorageOpenV1Application.java
@@ -0,0 +1,13 @@
+package com.example.StorageOpenV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageOpenV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageOpenV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StorageOpenV1/StorageOpenV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StorageOpenV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageOpenV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
